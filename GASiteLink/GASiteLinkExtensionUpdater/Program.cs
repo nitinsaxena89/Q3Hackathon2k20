d0eec6ad04e3667fb788a79b10e6da283d7ea796
Nitin Saxena <nitin.s@justanswer.com>
2020-08-07 20:24:18 +0530

    Test Commit - Fetch SiteLinks

--- GASiteLink/GASiteLinkExtensionUpdater/Program.cs
+++ GASiteLink/GASiteLinkExtensionUpdater/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Google.Api.Ads.AdWords.Lib;
+using Google.Api.Ads.AdWords.v201809;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,24 @@ namespace GASiteLinkExtensionUpdater
     {
         static void Main(string[] args)
         {
+            AdWordsUser adwordsUser = new AdWordsUser();
+            CampaignExtensionSettingService campaignExtensionSettingService = (CampaignExtensionSettingService)adwordsUser.GetService(AdWordsService.v201809.CampaignExtensionSettingService);
+            //retrivew siteLink
+            Selector selector = new Selector()
+            {
+                fields = new string[] {
+                    CampaignExtensionSetting.Fields.ExtensionType,
+                    ExtensionSetting.Fields.Extensions
+                },
+                predicates = new Predicate[] {
+                    Predicate.Equals(CampaignExtensionSetting.Fields.ExtensionType,
+                    FeedType.SITELINK.ToString()),
+  },
+                paging = Paging.Default
+            };
+
+            CampaignExtensionSettingPage page = campaignExtensionSettingService.get(selector);
+            CampaignExtensionSetting campaignExtensionSetting = page.entries[0];
         }
     }
 }
